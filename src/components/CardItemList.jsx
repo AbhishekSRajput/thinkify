898bb272178ab0be4699ac4177940c52f9bb8fef
abhishek singh <abhisingh3@hotmail.com>
2024-10-09 22:25:15 +0530

    bug/once image is shown should not register click event

--- src/components/CardItemList.jsx
+++ src/components/CardItemList.jsx
@@ -1,45 +1,65 @@
 import { CardItem } from "./CardItem";
 import GameData from "../app.mock";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const CardItemList = () => {
-	const [cardList, setCardList] = useState([...GameData]);
+    const [cardList, setCardList] = useState([...GameData]);
+	const [selectedCards, setSelectedCards] = useState([]);
+	const [matchedCards, setMatchedCards] = useState(new Set());
+
 	const onClickHandler = (currentId) => {
-		const filteredCard = cardList.filter((item) => {
-			if (item.id === currentId) {
-				item.isOpen = true;
+		const clickedCard = cardList.find((card) => card.id === currentId);
+
+		if (clickedCard.isOpen || matchedCards.has(currentId)) {
+			return;
+		}
+
+		setCardList((prevCards) =>
+			prevCards.map((card) =>
+				card.id === currentId ? { ...card, isOpen: true } : card
+			)
+		);
+		setSelectedCards((prev) => [...prev, currentId]);
+
+		if (selectedCards.length === 1) {
+			const firstCardId = selectedCards[0];
+			const firstCard = cardList.find((card) => card.id === firstCardId);
+
+			if (firstCard.name !== clickedCard.name) {
+				setTimeout(() => {
+					setCardList((prevCards) =>
+						prevCards.map((card) =>
+							card.id === currentId || card.id === firstCardId
+								? { ...card, isOpen: false }
+								: card
+						)
+					);
+				}, 500);
+			} else {
+				setMatchedCards(
+					(prev) => new Set([...prev, firstCardId, currentId])
+				);
 			}
-		});
-
-		setCardList((prevState) => {
-			return [...prevState, ...filteredCard];
-		});
-
-		setTimeout(() => {
-			setCardList((prevState) => {
-				return prevState.map((item) => {
-					return {
-						...item,
-						isOpen: false,
-					};
-				});
-			});
-		}, 500);
+		}
 	};
+
+	useEffect(() => {
+		if (selectedCards.length === 2) {
+			setSelectedCards([]);
+		}
+	}, [selectedCards]);
 
 	return (
 		<div className='card-item-list'>
-			{cardList.map((item) => {
-				return (
-					<CardItem
-						key={item.id}
-						id={item.id}
-						image={item.pic}
-						onClick={onClickHandler}
-						isOpen={item.isOpen}
-					></CardItem>
-				);
-			})}
+			{cardList.map((item) => (
+				<CardItem
+					key={item.id}
+					id={item.id}
+					image={item.pic}
+					onClick={onClickHandler}
+					isOpen={item.isOpen}
+				/>
+			))}
 		</div>
 	);
 };
